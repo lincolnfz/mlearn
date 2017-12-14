3110fe77a9eae95b279737b1a4b67fbc7d314deb
lincoln <lincolnfz@gmail.com>
2017-12-14 20:16:10 +0800

    多层网络还有问题

--- bp-network.py
+++ bp-network.py
@@ -14,11 +14,13 @@ from matplotlib.colors import colorConverter, ListedColormap
 from mpl_toolkits.mplot3d import Axes3D  
 from matplotlib import cm
 
+samples = 100
 # Generate the dataset
-X, t = sklearn.datasets.make_circles(n_samples=100, shuffle=False, factor=0.3, noise=0.1)
-T = np.zeros((100,2)) # Define target matrix
+X, t = sklearn.datasets.make_circles(n_samples=samples, shuffle=False, factor=0.3, noise=0.1)
+T = np.zeros((samples,2)) # Define target matrix
 T[t==1,1] = 1
 T[t==0,0] = 1
+
 # Separate the red and blue points for plotting
 x_red = X[t==0]
 x_blue = X[t==1]
@@ -39,7 +41,7 @@ plt.show()
 
 
 # Define the logistic function
-def logistic(z): 
+def logistic(z):
     return 1 / (1 + np.exp(-z))
 
 # Define the softmax function
@@ -62,3 +64,161 @@ def nn(X, Wh, bh, Wo, bo):
 #  1 or 0 depending on the predicted class
 def nn_predict(X, Wh, bh, Wo, bo): 
     return np.around(nn(X, Wh, bh, Wo, bo))
+
+
+#输出层
+# Define the cost function
+def cost(Y, T):
+    return - np.multiply(T, np.log(Y)).sum()
+
+# Define the error function at the output
+def error_output(Y, T):
+    return Y - T
+
+# Define the gradient function for the weight parameters at the output layer
+def gradient_weight_out(H, Eo): 
+    return  H.T.dot(Eo)
+
+# Define the gradient function for the bias parameters at the output layer
+def gradient_bias_out(Eo): 
+    return  np.sum(Eo, axis=0, keepdims=True)
+
+
+#隐藏层
+# Define the error function at the hidden layer
+def error_hidden(H, Wo, Eo):
+    # H * (1-H) * (E . Wo^T)
+    return np.multiply(np.multiply(H,(1 - H)), Eo.dot(Wo.T))
+
+# Define the gradient function for the weight parameters at the hidden layer
+def gradient_weight_hidden(X, Eh):
+    return X.T.dot(Eh)
+
+# Define the gradient function for the bias parameters at the output layer
+def gradient_bias_hidden(Eh): 
+    return  np.sum(Eh, axis=0, keepdims=True)
+
+# Initialize weights and biases
+init_var = 1
+# Initialize hidden layer parameters
+bh = np.random.randn(1, 3) * init_var
+Wh = np.random.randn(2, 3) * init_var
+# Initialize output layer parameters
+bo = np.random.randn(1, 2) * init_var
+Wo = np.random.randn(3, 2) * init_var
+
+# Compute the gradients by backpropagation
+# Compute the activations of the layers
+H = hidden_activations(X, Wh, bh)
+Y = output_activations(H, Wo, bo)
+# Compute the gradients of the output layer
+Eo = error_output(Y, T)
+JWo = gradient_weight_out(H, Eo)
+Jbo = gradient_bias_out(Eo)
+# Compute the gradients of the hidden layer
+Eh = error_hidden(H, Wo, Eo)
+JWh = gradient_weight_hidden(X, Eh)
+Jbh = gradient_bias_hidden(Eh)
+
+
+raise ValueError('123')
+
+#检查参数
+# Combine all parameter matrices in a list
+params = [Wh, bh, Wo, bo]
+# Combine all parameter gradients in a list
+grad_params = [JWh, Jbh, JWo, Jbo]
+
+# Set the small change to compute the numerical gradient
+eps = 0.0001
+
+# Check each parameter matrix
+for p_idx in range(len(params)):
+    # Check each parameter in each parameter matrix
+    for row in range(params[p_idx].shape[0]):
+        for col in range(params[p_idx].shape[1]):
+            # Copy the parameter matrix and change the current parameter slightly
+            p_matrix_min = params[p_idx].copy()
+            p_matrix_min[row,col] -= eps
+            p_matrix_plus = params[p_idx].copy()
+            p_matrix_plus[row,col] += eps
+            # Copy the parameter list, and change the updated parameter matrix
+            params_min = params[:]
+            params_min[p_idx] = p_matrix_min
+            params_plus = params[:]
+            params_plus[p_idx] =  p_matrix_plus
+            # Compute the numerical gradient
+            grad_num = (cost(nn(X, *params_plus), T)-cost(nn(X, *params_min), T))/(2*eps)
+            # Raise error if the numerical grade is not close to the backprop gradient
+            if not np.isclose(grad_num, grad_params[p_idx][row,col]):
+                raise ValueError('Numerical gradient of {:.6f} is not close to the backpropagation gradient of {:.6f}!'.format(float(grad_num), float(grad_params[p_idx][row,col])))
+print('No gradient errors found')
+
+# Define the update function to update the network parameters over 1 iteration
+def backprop_gradients(X, T, Wh, bh, Wo, bo):
+    # Compute the output of the network
+    # Compute the activations of the layers
+    H = hidden_activations(X, Wh, bh)
+    Y = output_activations(H, Wo, bo)
+    # Compute the gradients of the output layer
+    Eo = error_output(Y, T)
+    JWo = gradient_weight_out(H, Eo)
+    Jbo = gradient_bias_out(Eo)
+    # Compute the gradients of the hidden layer
+    Eh = error_hidden(H, Wo, Eo)
+    JWh = gradient_weight_hidden(X, Eh)
+    Jbh = gradient_bias_hidden(Eh)
+    return [JWh, Jbh, JWo, Jbo]
+
+def update_velocity(X, T, ls_of_params, Vs, momentum_term, learning_rate):
+    # ls_of_params = [Wh, bh, Wo, bo]
+    # Js = [JWh, Jbh, JWo, Jbo]
+    Js = backprop_gradients(X, T, *ls_of_params)
+    return [momentum_term * V - learning_rate * J for V,J in zip(Vs, Js)]
+
+def update_params(ls_of_params, Vs):
+    # ls_of_params = [Wh, bh, Wo, bo]
+    # Vs = [VWh, Vbh, VWo, Vbo]
+    return [P + V for P,V in zip(ls_of_params, Vs)]
+
+# Run backpropagation
+# Initialize weights and biases
+init_var = 0.1
+# Initialize hidden layer parameters
+bh = np.random.randn(1, 3) * init_var
+Wh = np.random.randn(2, 3) * init_var
+# Initialize output layer parameters
+bo = np.random.randn(1, 2) * init_var
+Wo = np.random.randn(3, 2) * init_var
+# Parameters are already initilized randomly with the gradient checking
+# Set the learning rate
+learning_rate = 0.02
+momentum_term = 0.9
+
+# define the velocities Vs = [VWh, Vbh, VWo, Vbo]
+Vs = [np.zeros_like(M) for M in [Wh, bh, Wo, bo]]
+
+# Start the gradient descent updates and plot the iterations
+nb_of_iterations = 600  # number of gradient descent updates
+lr_update = learning_rate / nb_of_iterations # learning rate update rule
+ls_costs = [cost(nn(X, Wh, bh, Wo, bo), T)]  # list of cost over the iterations
+for i in range(nb_of_iterations):
+    # Update the velocities and the parameters
+    Vs = update_velocity(X, T, [Wh, bh, Wo, bo], Vs, momentum_term, learning_rate)
+    Wh, bh, Wo, bo = update_params([Wh, bh, Wo, bo], Vs)
+    ls_costs.append(cost(nn(X, Wh, bh, Wo, bo), T))
+    #print(cost(nn(X, Wh, bh, Wo, bo), T))
+    
+# Plot the cost over the iterations
+plt.plot(ls_costs, 'b-')
+plt.xlabel('iteration')
+plt.ylabel('$\\xi$', fontsize=15)
+plt.title('Decrease of cost over backprop iteration')
+plt.grid()
+plt.show()
+
+print( 'wh', Wh,  '\nbh', bh, '\nWo', Wo, '\nbo', bo )
+
+print(cost(nn(X, Wh, bh, Wo, bo), T))
+
+

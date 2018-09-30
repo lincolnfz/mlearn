0c880471276022c608ae6066ff45f93c8ea682e1
lincolnfz <lincolnfz@gmail.com>
2018-10-01 01:58:21 +0800

    nlp test

---
+++ wordnlp.py
@@ -0,0 +1,20 @@
+import gensim
+import jieba
+import os
+
+class MySentences(object):
+    def __init__(self, dirname):
+        self.dirname = dirname
+ 
+    def __iter__(self):
+        for fname in os.listdir(self.dirname):
+            for line in open(os.path.join(self.dirname, fname)):
+                yield line, line.split()
+ 
+sentences = MySentences('./nlp-txt') # a memory-friendly iterator
+for line, split in sentences:
+    seg_list = jieba.cut(line, cut_all=False)
+    a = map( lambda x: print(x), seg_list )
+    print(list(a))
+    #print(type(a))
+#model = gensim.models.Word2Vec(sentences)
